e309821e158c3f956122e7fd70106c7885484679
magouwu <themagosiniestro@gmail.com>
2022-05-07 01:15:35 -0500

    second file commited

--- main.js
+++ main.js
@@ -1,17 +1,36 @@
-var maxCount = 150;
-const counter = document.querySelector('.container-contact .counter');
-const limitedtextarea = document.querySelector('#msg');
+const remainingCharsText = document.getElementById('counter')
+const myTextArea = document.getElementById('mytextarea')
+const MAX_CHARS = 150;
+ 
+myTextArea.addEventListener('input', function(e) {
+  const remaining = MAX_CHARS - myTextArea.value.length;
+  remainingCharsText.textContent=remaining;
+  if(remaining <=0){
+    e.preventDefault();
+    return;
+  }else{
+ 
+  const color = remaining < MAX_CHARS * 0.1 ? 'red' : null;
+  console.log(remaining);
+  
+  remainingCharsText.style.color = color;
+  }
 
-limitedtextarea.addEventListener('keydown', function(e){
-    if (maxCount === 0 && e.key !== 'Backspace' || maxCount === 150 && e.key === 'Backspace') {
-        e.preventDefault();
-        return;
-      }
-      if (e.key === 'Backspace') {
-        maxCount = maxCount + 1;
-        counter.innerHTML = maxCount;
-      } else {
-        maxCount = maxCount - 1;
-        counter.innerHTML = maxCount;
-      }
-    });
+});
+/*
+myTextArea.addEventListener('keydown', function(e){
+  const remaining = MAX_CHARS - myTextArea.value.length;
+  remainingCharsText.textContent=remaining;
+
+if (remaining <= 0 && e.key !== 'Backspace' || remaining === 150 && e.key === 'Backspace') {
+  e.preventDefault();
+  return;
+}
+if (e.key === 'Backspace') {
+  remaining = remaining + 1;
+  remainingCharsText.innerHTML = remaining;
+} else {
+  remaining = remaining - 1;
+  remainingCharsText.innerHTML = remaining;
+}
+});*/
